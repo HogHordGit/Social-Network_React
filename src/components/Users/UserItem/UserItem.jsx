cbdd23fae8bcd52cd3417736c45f0e5488e241cf
Vladymyr <hoghord@gmail.com>
2023-12-25 16:08:38 +0200

    save commit

--- src/components/Users/UserItem/UserItem.jsx
+++ src/components/Users/UserItem/UserItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import userPhoto from "../../../assets/img/img.png";
 
 const UserItem = (props) => {
 
@@ -7,7 +8,7 @@ const UserItem = (props) => {
 
             <div className="uItem__photo">
                 <div className="uItem__image">
-                    <img src={props.user.photoUrl} alt="face"/>
+                    <img src={props.user.photos.large != null ? props.user.photos.small : userPhoto} alt="avatar"/>
                 </div>
                 <div className="uItem__button">
                     {props.user.followed ? <button onClick={() => {
@@ -22,7 +23,7 @@ const UserItem = (props) => {
                 <div className="uItem__about">
                     <div className="uItem__aboutBody">
                         <div className="uItem__name">
-                            <span>{props.user.fullName}.</span>
+                            <span>{props.user.name}.</span>
                         </div>
                         <div className="uItem__message">
                             <span>{props.user.status}</span>
@@ -30,7 +31,7 @@ const UserItem = (props) => {
                     </div>
                 </div>
                 <div className="uItem__location">
-                    <span>{props.user.location.city}, {props.user.location.country}</span>
+                    <span>city here, country here</span>
                 </div>
             </div>
 
